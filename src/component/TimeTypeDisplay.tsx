176523e2b1c3e23201717288d4df31bbd7874402
Anthony Lee <hososlch@gmail.com>
2023-08-15 11:44:39 +0800

    feat: TimeTypeDisplay

--- src/component/TimeTypeDisplay.tsx
+++ src/component/TimeTypeDisplay.tsx
@@ -15,10 +15,11 @@ export const TimeTypeDisplay = React.memo<Props>(({type}) => {
             bgColor={type === "天顯時格" ? "pink.50" : type === "五不遇時" ? "gray.50" : type === "時干入墓" ? "green.50" : "white"}
             color={type === "天顯時格" ? "pink.500" : type === "五不遇時" ? "gray.500" : type === "時干入墓" ? "green.500" : "gray.500"}
             borderBottomColor={type === "天顯時格" ? "pink.200" : type === "五不遇時" ? "gray.200" : type === "時干入墓" ? "green.200" : "gray.200"}
-            borderBottomWidth={type === null ? 0 : 1}
+            borderBottomWidth={type === null ? undefined : {base: 1, md: 2}}
             borderBottomStyle="dashed"
             fontWeight="bold"
-            p={1}
+            fontSize={{base: "md", md: "xl"}}
+            p={{base: 1, md: 2}}
         >
             {type || <React.Fragment>&nbsp;</React.Fragment>}
         </Flex>

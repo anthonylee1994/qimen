9c1b1e4b85fd12bbdfca6e950b6526ac90c9f876
Anthony Lee <hososlch@gmail.com>
2023-08-15 11:36:53 +0800

    feat: DatePicker

--- src/component/DatePicker.tsx
+++ src/component/DatePicker.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Flex, IconButton, Input, Tooltip} from "@chakra-ui/react";
+import {Flex, IconButton, Input, Tooltip, useBreakpointValue} from "@chakra-ui/react";
 import {ArrowBackIcon, ArrowForwardIcon, CalendarIcon} from "@chakra-ui/icons";
 import moment from "moment";
 
@@ -9,10 +9,15 @@ interface Props {
 }
 
 export const DatePicker = React.memo<Props>(({date, setDate}) => {
+    const buttonSize = useBreakpointValue({base: "md", md: "lg"});
+    const iconSize = useBreakpointValue({base: "lg", md: "2xl"});
+
     return (
-        <Flex w="full" p={1} bgColor="gray.300" justifyContent="center">
-            <IconButton aria-label="left" icon={<ArrowBackIcon />} mr={1.5} onClick={() => setDate(new Date(date.getTime() - 120 * 60 * 1000))} />
+        <Flex w="full" p={{base: 1, md: 2}} bgColor="gray.300" justifyContent="center">
+            <IconButton size={buttonSize} aria-label="left" icon={<ArrowBackIcon fontSize={iconSize} />} mr={1.5} onClick={() => setDate(new Date(date.getTime() - 120 * 60 * 1000))} />
             <Input
+                size={buttonSize}
+                fontSize={{base: "md", md: "xl"}}
                 maxWidth={400}
                 textAlign="center"
                 borderRadius="md"
@@ -23,9 +28,9 @@ export const DatePicker = React.memo<Props>(({date, setDate}) => {
                 onChange={e => setDate(new Date(e.target.value))}
             />
             <Tooltip label="當前時間">
-                <IconButton aria-label="current" icon={<CalendarIcon />} ml={1.5} onClick={() => setDate(new Date())} />
+                <IconButton size={buttonSize} aria-label="current" icon={<CalendarIcon fontSize={iconSize} />} ml={1.5} onClick={() => setDate(new Date())} />
             </Tooltip>
-            <IconButton aria-label="right" icon={<ArrowForwardIcon />} ml={1.5} onClick={() => setDate(new Date(date.getTime() + 120 * 60 * 1000))} />
+            <IconButton size={buttonSize} aria-label="right" icon={<ArrowForwardIcon fontSize={iconSize} />} ml={1.5} onClick={() => setDate(new Date(date.getTime() + 120 * 60 * 1000))} />
         </Flex>
     );
 });
